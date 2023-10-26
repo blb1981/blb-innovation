152c065664500c594f74a5035b23e4f40cfdf5a5
Brandon Bland <brandonbland90@gmail.com>
2023-10-26 12:05:41 -0400

    Fix path for dotenv file, build for production

--- src/email.php
+++ src/email.php
@@ -8,7 +8,7 @@ use Dotenv\Dotenv;
 
 require './vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable('../');
 $dotenv->load();
 
 echo $_ENV['EMAIL_SUBJECT'];
