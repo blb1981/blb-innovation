91b7c204ae88210904f1dd030751950f5292721a
Brandon Bland <brandonbland90@gmail.com>
2023-10-26 11:05:23 -0400

    Move unneeded files to v1 folder, customize build to include files for contact form

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 const path = require('path')
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
   },
   server: {
-    port: 8080
-  }
+    port: 8080,
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'email-sent.html',
+          dest: '.',
+        },
+        {
+          src: 'email.php',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
 }
